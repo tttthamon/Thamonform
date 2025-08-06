483d3e4a2037f211979b5714188bc033c996f803
tttthamon <thamonnuanmuak@gmail.com>
2025-08-06 16:27:08 +0700

    ee

--- index.js
+++ index.js
@@ -9,36 +9,36 @@ document.getElementById('bookingForm').addEventListener('submit', function(e) {
   const errormessage = document.getElementById('errormessage');
   errormessage.textContent = '';
 
-  // ตรวจชื่อ-สกุล
+ 
   if (name.split(' ').length < 2) {
     errormessage.textContent = 'กรุณากรอกชื่อ-นามสกุลให้ครบ (อย่างน้อย 2 คำ)';
     return;
   }
 
-  // ตรวจเบอร์โทร
+  
   const telRegex = /^0\d{9}$/;
   if (!telRegex.test(tel)) {
     errormessage.textContent = 'กรุณากรอกเบอร์โทรศัพท์ 10 หลักขึ้นต้นด้วย 0';
     return;
   }
 
-  // ตรวจจำนวนตั๋ว
+  
   if (isNaN(ticketCount) || ticketCount < 1 || ticketCount > 5) {
     errormessage.textContent = 'จำนวนตั๋วต้องระหว่าง 1 ถึง 5';
     return;
   }
 
-  // ตรวจเงื่อนไข VIP / Premium
+  
   if ((ticketType === 'vip' || ticketType === 'premium') && ticketCount > 2) {
     errormessage.textContent = 'ประเภท VIP หรือ Premium จำกัดไม่เกิน 2 ใบ';
     return;
   }
 
-  // แสดง modal
+  
   document.getElementById('successModal').style.display = 'flex';
 });
 
-// ปิด modal
+
 document.getElementById('closeModal').addEventListener('click', function () {
   document.getElementById('successModal').style.display = 'none';
 });
